ddff4f4f522424edcd44df527b6e3c5a6102d64c
hassanahmad2136 <35006202+hassanahmad2136@users.noreply.github.com>
2024-06-23 20:35:03 +0500

    Update companydetailsscrape.py

--- companydetailsscrape.py
+++ companydetailsscrape.py
@@ -3,6 +3,7 @@ from datetime import datetime, timedelta
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
 from initdriver import get_driver
 import pandas as pd
 from parseurl import remove_url_parameters
@@ -11,30 +12,15 @@ from parseurl import remove_url_parameters
 data = pd.read_csv("companies_unique.csv")
 urls = data["Company Linkedin Link"]
 
-# Set cookie expiration
-expiry_date = datetime.now() + timedelta(days=60)
-expiry_timestamp = int(time.mktime(expiry_date.timetuple()))
-
-# li_at cookie value
-value = 'REPLACE_WITH_COOKIE_VALUE'
-
 # Initialize driver
 driver = get_driver()
 driver.get("https://www.linkedin.com/")
 
-# Add cookie
-li_at_cookie = {
-    'name': 'li_at',
-    'value': value,
-    'domain': '.linkedin.com',
-    'expiry': expiry_timestamp,
-    'path': '/',
-    'secure': True,
-    'httpOnly': True
-}
 
-driver.add_cookie(li_at_cookie)
-driver.refresh()
+while "feed" not in driver.current_url: pass
+
+print("Logged In")
+
 
 # Initialize lists
 webs = []
@@ -42,82 +28,89 @@ talent_roles = []
 
 # Iterate through URLs
 for url in urls:
+    time.sleep(2)
     url = remove_url_parameters(url)
-    url = url + "/about"
-    driver.execute_script(f"window.location.href='{url}'")
+    driver.get(url + "/about")
     print(f"Scraping: {url}")
     # Wait for the element to be present
     try:
-        web_element = WebDriverWait(driver, 20).until(
+        web_element = WebDriverWait(driver, 30).until(
             EC.presence_of_element_located((By.CSS_SELECTOR, "dd a span"))
         )
         web = web_element.text
     except Exception as e:
         web = ""
-        print(f"Error: {e}")
-
+        print(f"Error1: {e}")
+        continue
+    time.sleep(1)
+    driver.get(url+"/people")
+    links = []
+    time.sleep(1)
+    if "talent" not in driver.current_url:
+        try:
+            value = "Search employees by title, keyword or school"
+            input = WebDriverWait(driver, 30).until(
+                EC.presence_of_element_located(
+                    (By.XPATH, f"//input[@placeholder='{value}']")
+)
+            )
+            driver.execute_script("arguments[0].scrollIntoView()", input)
+            time.sleep(1)
+            input.send_keys("talent")
+            input.send_keys(Keys.ENTER)
+            value = "Keyword search already applied"
+            web_element = WebDriverWait(driver, 30).until(
+                EC.presence_of_element_located(
+                    (By.XPATH, f"//input[@placeholder='{value}']")
+)
+            )
+        except Exception as e:
+            print(e)
+            continue
     try:
-        about_button = WebDriverWait(driver, 20).until(
-            EC.element_to_be_clickable((By.CSS_SELECTOR, "span.t-normal.t-black--light.link-without-visited-state.link-without-hover-state"))
+        web_element = WebDriverWait(driver, 30).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "li.grid.grid__col--lg-8.block.org-people-profile-card__profile-card-spacing"))
         )
-        cs = driver.find_elements(By.TAG_NAME, "a")
-        for c in cs:
-            href = c.get_attribute("href")
-            print(href)
-            if href and '/search/result' in href and 'people' in href:
-                driver.execute_script(f"window.location.href='{href}'")
-                break
     except Exception as e:
         print(e)
-
-    links = []
-    current_page = 1
-    while True:
-        try:
-            WebDriverWait(driver, 20).until(
-                EC.presence_of_all_elements_located((By.CSS_SELECTOR, "div.entity-result__primary-subtitle.t-14.t-black.t-normal"))
-            )
-            persons = driver.find_elements(By.CSS_SELECTOR, "div.entity-result__primary-subtitle.t-14.t-black.t-normal")
-            profiles = driver.find_elements(By.CSS_SELECTOR,"span a.app-aware-link")
-            for person in range(len(persons)):
-                if "talent" in persons[person].text or 'Talent' in persons[person].text:
-                    if "search/result" in profiles[person].get_attribute("href"):
-                        continue
-                    links.append(profiles[person].get_attribute("href"))
-                pass
-        except Exception as e:
-            print(f"Error: {e}")
-
-        current_page += 1
-        try:
-            driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        except:
-            pass
-        time.sleep(1)
-
-        try:
-            next_button = WebDriverWait(driver, 20).until(
-                EC.presence_of_element_located((By.XPATH, "//button[contains(@class, 'artdeco-pagination__button--next')]"))
-            )
-            if 'artdeco-button--disabled' in next_button.get_attribute('class'):
-                talent_roles.append(links)
-                break
-
-            else:
-                new_url = href + f"&page={current_page}"
-
-                driver.execute_script(f"window.location.href='{new_url}'")
-        except Exception as e:
-            print(f"Error: {e}")
-            talent_roles.append(links)
+        continue
+    li = 0
+    while 1:
+        time.sleep(2)
+        cards = driver.find_elements(By.CSS_SELECTOR,"li.grid.grid__col--lg-8.block.org-people-profile-card__profile-card-spacing")
+        new_cards = cards[li:]
+        if li == len(cards):
             break
-
+        for card in new_cards:
+            li+=1
+            driver.execute_script("arguments[0].scrollIntoView()",card)
+            role = card.find_element(By.CSS_SELECTOR,"div.ember-view.lt-line-clamp.lt-line-clamp--multi-line")
+            try:
+                link = card.find_element(By.TAG_NAME,"a").get_attribute("href")
+            except:
+                continue
+            if "talent" in role.text.lower():
+                print(link)
+                links.append(link)
+        button = driver.find_elements(By.XPATH,
+                                      "//*[self::div or self::button or self::span][contains(text(), 'See more')]")
+        if len(button) != 0:
+            driver.execute_script("arguments[0].click();", button[0])
+            time.sleep(1)
+    talent_roles.append(links)
     webs.append(web)
 
 # Save results to CSV
 df = pd.read_csv("companies_unique.csv")
 df['Website Link'] = pd.Series(webs)
-df['Profile Links'] = pd.Series(talent_roles)
 output_csv_file_path = 'companies_unique.csv'
+max_len = max(len(sublist) for sublist in talent_roles)
+
+column_names = [f'Column {i+4}' for i in range(max_len)]
+
+talent_roles_df = pd.DataFrame(talent_roles, columns=column_names)
+
+df = pd.concat([df, talent_roles_df], axis=1)
+
 df.to_csv(output_csv_file_path, index=False)
 driver.quit()
